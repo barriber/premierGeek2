e4d196c1f6f476c94da96ec6b128fcdeb8f9cdd0
Bari Berezovsky <boris.berezovsky@safebreach.com>
2018-05-21 14:11:14 +0300

    bets

--- server/bets.js
+++ server/bets.js
@@ -2,6 +2,20 @@ import {success} from './libs/response';
 import con from './libs/db';
 
 
-export function makeBet(event, context, callback) {
+export function placeBet(event, context, callback) {
+    const {fixtureId, homeTeamScore, awayTeamScore} = JSON.parse(event.body);
+    const userId = event.requestContext.identity.cognitoIdentityId;
+    const sql = `REPLACE INTO bets (fixtureId, userId, time, homeTeamScore, awayTeamScore)
+     Values (${fixtureId}, ${userId}, ${Date.now()}, ${homeTeamScore}, ${awayTeamScore})`;
+    con.query(sql, function(err, result) {
+        console.log('--------');
+        if (err) {
+            console.log('*****************');
+            throw err;
+        };
+
+        con.end();
+        callback(null, success())
+    })
 
 }
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,18 +1,7 @@
-const success = require('./libs/response').success;
-const mysql = require('mysql');
+import  { success } from './libs/response';
+import con from './libs/db';
 
-let con = mysql.createPool({
-    connectionLimit : 5,
-    host : 'premiergeek.c1t0nwcytv3y.us-east-1.rds.amazonaws.com',
-    user : 'noam',
-    password : '12345678',
-    database : 'noam',
-});
-
-module.exports.main = (event, context, callback) => {
-    // console.log('CONTEXT -START')
-    // console.log(JSON.stringify(context));
-    // console.log('CONTEXT -END');
+export function main(event, context, callback) {
     console.log('EVENT -START')
     console.log(JSON.stringify(event));
     console.log('EVENT -END');
@@ -32,70 +21,3 @@ module.exports.main = (event, context, callback) => {
         callback(null, success(result));
     });
 }
-
-
-// const teams = async function() {
-//     const response = await axios.get('http://api.football-data.org/v1/competitions/467/teams', {
-//         headers: {
-//             'X-Auth-Token': '5aab4c2c6c8a4af188e5be626459fb78',
-//         },
-//     });
-//
-//     const {teams} = response.data;
-//     const x = teams.map(team => {
-//         const {name, crestUrl: logo, _links} = team;
-//         const link = _links.self.href;
-//         const split = link.split('/');
-//         const id = parseInt(split[split.length - 1]);
-//         return {
-//             name,
-//             logo,
-//             id,
-//         }
-//     });
-//
-//     const promises = x.map(team => {
-//         return con.query('INSERT INTO Teams SET ?', team)
-//     })
-//
-//     const y = await Promise.all(promises);
-//     con.end();
-//     callback(null, success(x));
-//
-// }
-
-
-
-
-// const response = await axios.get('http://api.football-data.org/v1/competitions/467/fixtures', {
-//     headers: {
-//         'X-Auth-Token': '5aab4c2c6c8a4af188e5be626459fb78',
-//     },
-// });
-//
-// const {fixtures} = response.data;
-// const x = fixtures.map(fixture => {
-//     const {date, status, matchday: round, _links: link} = fixture;
-//     const awayUrl = link.awayTeam.href.split('/');
-//     const homeUrl = link.homeTeam.href.split('/');
-//     const split = link.self.href.split('/');
-//     const awayTeamId = parseInt(awayUrl[awayUrl.length - 1]);
-//     const homeTeamId = parseInt(homeUrl[homeUrl.length - 1]);
-//     const id = parseInt(split[split.length - 1]);
-//     return {
-//         id,
-//         date,
-//         status,
-//         round,
-//         awayTeamId,
-//         homeTeamId,
-//     }
-// });
-//
-// const promises = x.map(fixture => {
-//     return con.query('INSERT INTO fixtures SET ?', fixture)
-// });
-//
-// const y = await Promise.all(promises);
-// con.end();
-// callback(null, success(x));
